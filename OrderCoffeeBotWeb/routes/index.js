a9c4217a1fc30cae92a4366a4b41c88ad96886a2
stevkan <skanberg@kanbergs.com>
2018-04-05 12:26:13 -0700

    added terms of use page

--- OrderCoffeeBotWeb/routes/index.js
+++ OrderCoffeeBotWeb/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Coffee Bot' });
 });
 
+router.get('/terms', function (req, res) {
+    res.render('terms', { title: 'Terms of Use' });
+});
+
 module.exports = router;
